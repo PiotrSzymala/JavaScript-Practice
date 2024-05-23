1b8b975244f702a10cdadc1463fa90d83b1c69d2
Piotr Szymala <p.szymala@sellpander.pl>
2024-05-23 16:48:39 +0200

    Refactor code

--- UserFetching/file.js
+++ UserFetching/file.js
@@ -1,15 +1,15 @@
-window.onload = () => {
- const tableBody = document.querySelector("table tbody") 
+window.onload = async () => {
+    const tableBody = document.querySelector("table tbody")
     const apiBaseUrl = "https://users-api-jk.azurewebsites.net"
-    fetch(`${apiBaseUrl}/api/Users`)
-    .then(response => {
-        console.log(response);
 
-        return response.json()
-    }).then(data => {
-        console.log(data);
-        const userRows = data.map(user => {
-            return `<tr>
+    const response = await fetch(`${apiBaseUrl}/api/Users`)
+    console.log(response);
+
+    const data = await response.json()
+    console.log(data);
+    
+    const userRows = data.map(user => {
+        return `<tr>
             <td>${user.id}</td>
             <td>${user.name}</td>
             <td>${user.email}</td>
@@ -17,8 +17,27 @@ window.onload = () => {
             <td>${user.address.city}</td>
             <td>${user.address.zipCode}</td>
             </tr>`
-        })
-
-        tableBody.innerHTML = userRows.join("")
     })
+
+    tableBody.innerHTML = userRows.join("")
+    
+    // .then(response => {
+    //     console.log(response);
+
+    //     return response.json()
+    // }).then(data => {
+    //     console.log(data);
+    //     const userRows = data.map(user => {
+    //         return `<tr>
+    //         <td>${user.id}</td>
+    //         <td>${user.name}</td>
+    //         <td>${user.email}</td>
+    //         <td>${user.address.street}</td>
+    //         <td>${user.address.city}</td>
+    //         <td>${user.address.zipCode}</td>
+    //         </tr>`
+    //     })
+
+    //     tableBody.innerHTML = userRows.join("")
+    // })
 }
